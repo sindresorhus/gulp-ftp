5dbe9d6df5ee27ed421b9bbaa40bd31bd76f218c
Sindre Sorhus <sindresorhus@gmail.com>
2014-01-19 17:29:58 +0100

    switch to through2

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 'use strict';
 var path = require('path');
-var es = require('event-stream');
 var gutil = require('gulp-util');
+var through = require('through2');
 var JSFtp = require('jsftp');
 
 JSFtp = require('jsftp-mkdirp')(JSFtp);
 
 module.exports = function (options) {
 	if (options.host === undefined) {
-		throw new Error('`host` required.');
+		throw new gutil.PluginError('gulp-ftp', '`host` required.');
 	}
 
 	var fileCount = 0;
 	var remotePath = options.remotePath || '';
 	delete options.remotePath;
 
-	var stream = es.map(function (file, cb) {
+	return through.obj(function (file, enc, cb) {
+		if (file.isNull()) {
+			this.push(file);
+			return cb();
+		}
+
+		if (file.isStream()) {
+			this.emit('error', new gutil.PluginError('gulp-ftp', 'Streaming not supported'));
+			return cb();
+		}
+
 		// have to create a new connection for each file otherwise they conflict
 		var ftp = new JSFtp(options);
 		var relativePath = file.path.replace(file.cwd + '/', '');
@@ -23,28 +33,30 @@ module.exports = function (options) {
 
 		ftp.mkdirp(path.dirname(finalRemotePath), function (err) {
 			if (err) {
-				return cb(err);
+				this.emit('error', new gutil.PluginError('gulp-ftp', err));
+				return cb();
 			}
 
 			ftp.put(file.contents, finalRemotePath, function (err) {
 				if (err) {
-					return cb(err);
+					this.emit('error', new gutil.PluginError('gulp-ftp', err));
+					return cb();
 				}
 
 				fileCount++;
 				ftp.raw.quit();
-				cb(null, file);
+				cb();
 			});
 		});
-	});
 
-	stream.on('end', function () {
+		this.push(file);
+	}, function (cb) {
 		if (fileCount > 0) {
 			gutil.log('gulp-ftp:', gutil.colors.green(fileCount, fileCount === 1 ? 'file' : 'files', 'uploaded successfully'));
 		} else {
 			gutil.log('gulp-ftp:', gutil.colors.yellow('No files uploaded'));
 		}
-	});
 
-	return stream;
+		cb();
+	});
 };
